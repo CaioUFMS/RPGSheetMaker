9bfb7ff3466b08f9fc5ed3e74c52b2a1578a306c
Caio Rosa Carvalho <caio.carvalho@grupocard.com.br>
2022-11-16 09:48:05 -0400

    Started implementation on Skill

--- RPGSheetMaker/src/Skill.java
+++ RPGSheetMaker/src/Skill.java
@@ -1,4 +1,12 @@
 
 public class Skill {
-	//TODO Class stub
+	//Largely TODO
+	
+	//Definition of the Skill
+	private String name;
+	private String description;
+	
+	//This will be used to determine the base plus to the skill
+	private String associatedStat;
+	
 }
